e2431c61230c99082fce784ca886866364f62b84
Zach Lucas <zlucas@grubhub.com>
2019-04-11 11:03:05 -0400

    First commit

---
+++ FantasySurvivor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantasySurvivor
+//
+//  Created by Zach Lucas on 4/11/19.
+//  Copyright © 2019 Zach Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
